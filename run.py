f7615426dc9b472b8ee572e447dc9f9a623fb6f0
Jeremy Nelson <jermnelson@gmail.com>
2016-10-06 13:36:16 -0600

    added run for dispatcher

---
+++ run.py
@@ -0,0 +1,15 @@
+__author__ = "Jeremy Nelson"
+from werkzeug.serving import run_simple
+from werkzeug.wsgi import DispatcherMiddleware
+
+from app import app
+
+app.config['DEBUG'] = True
+
+application = DispatcherMiddleware(
+    app,
+    {"/fedora_utilities": app}
+)
+
+if __name__ == '__main__':
+    run_simple('0.0.0.0', 9455, application, use_reloader=True, use_debugger=True)
