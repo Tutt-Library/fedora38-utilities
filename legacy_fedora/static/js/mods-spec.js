996e88c952ceac5ec19365af5771ff06bd39dc6e
Jeremy Nelson <jermnelson@gmail.com>
2017-05-17 12:02:17 -0600

    Working MODS editor

--- legacy_fedora/static/js/mods-spec.js
+++ legacy_fedora/static/js/mods-spec.js
@@ -3,14 +3,14 @@ var modsSpec = {
         console.log("I have been changed now");
     },
     elements: {
-        "abstract": {
+        "mods:abstract": {
             menu: [{
                 caption: "Delete <abstract>",
                 action: Xonomy.deleteElement
             }],    
             hasText: true
         },
-        "accessCondition": {
+        "mods:accessCondition": {
             menu: [{
                 caption: "Add @type property",
                 action: Xonomy.newAttribute,
@@ -33,14 +33,14 @@ var modsSpec = {
                 }
             }
         },
-        "extent": {
+        "mods:extent": {
             hasText: true,
             menu: [{
                 caption: "Delete <extent>",
                 action: Xonomy.deleteElement
             }]
         },
-        "form": {
+        "mods:form": {
             hasText: true,
             menu: [{
                 caption: "Delete <form>",
@@ -61,81 +61,85 @@ var modsSpec = {
 
             }
         },
-        "language": {
+        "mods:language": {
             menu: [{
                 caption: "Add <languageTerm>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<languageTerm />"
+                actionParameter: "<mods:languageTerm xmlns:mods='http://www.loc.gov/mods/v3' />"
             },{
                 caption: "Delete <language>",
                 action: Xonomy.deleteElement                
             }],
             hasText: false
         },
-        "languageTerm": {
+        "mods:languageTerm": {
             menu: [{
                 caption: "Delete <languageTerm>",
                 action: Xonomy.deleteElement
             }],
             hasText: true
         },
-        "mods": {
+        "mods:mods": {
             menu: [{
                     caption: "Append <abstract>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<abstract/>"
+                    actionParameter: "<mods:abstract xmlns:mods='http://www.loc.gov/mods/v3'/>"
                 },
                 {
                     caption: "Append <accessCondition>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<accessCondition/>"
+                    actionParameter: "<mods:accessCondition xmlns:mods='http://www.loc.gov/mods/v3'/>"
                 },
                 {
                     caption: "Add <classification>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<classification/>"
+                    actionParameter: "<mods:classification xmlns:mods='http://www.loc.gov/mods/v3'/>"
                 },
                 {
                     caption: "Add <genre>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<genre/>"
+                    actionParameter: "<mods:genre xmlns:mods='http://www.loc.gov/mods/v3' />"
                 },{
                     caption: "Add <language>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<language><languageTerm /></language>"                
+                    actionParameter: "<mods:language xmlns:mods='http://www.loc.gov/mods/v3'><mods:languageTerm /></mods:language>"                
                 },{
                     caption: "Add <name>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<name><namePart/><role><roleTerm type=\"text\" authority=\"marcrelator\" /></role></name>"
+                    actionParameter: "<mods:name xmlns:mods='http://www.loc.gov/mods/v3'><mods:namePart/><mods:role><mods:roleTerm type=\"text\" authority=\"marcrelator\" /></mods:role></mods:name>"
                 },
                 {
                     caption: "Add <note>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<note></note>"
+                    actionParameter: "<mods:note xmlns:mods='http://www.loc.gov/mods/v3'></mods:note>"
                 },{
                     caption: "Add <physicalDescription>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<physicalDescription/>"
+                    actionParameter: "<mods:physicalDescription xmlns:mods='http://www.loc.gov/mods/v3'/>"
                 },{
                     caption: "Add <subject>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<subject />"
+                    actionParameter: "<mods:subject xmlns:mods='http://www.loc.gov/mods/v3' />"
                 },
                 {
                     caption: "Add <titleInfo>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<titleInfo><title/></titleInfo>"
+                    actionParameter: "<mods:titleInfo xmlns:mods='http://www.loc.gov/mods/v3'><mods:title/></mods:titleInfo>"
                 },
                 {
                     caption: "Add <topic>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<subject><topic /></subject>"
+                    actionParameter: "<mods:subject xmlns:mods='http://www.loc.gov/mods/v3'><mods:topic /></mods:subject>"
+                },
+                {
+                    caption: "Add <typeOfResource>",
+                    action: Xonomy.newElementChild,
+                    actionParameter: "<mods:typeOfResource xmlns:mods='http://www.loc.gov/mods/v3' />"
                 }
-
             ],
             hasText: false
         },
-        "classification": {
+        "mods:classification": {
             menu: [{
                 caption: "Add @authority attribute",
                 action: Xonomy.newAttribute,
@@ -160,7 +164,7 @@ var modsSpec = {
                 }
             }
         },
-        "dateCreated": {
+        "mods:dateCreated": {
             menu: [{
                 caption: "Add @keydate",
                 action: Xonomy.newAttribute,
@@ -180,7 +184,7 @@ var modsSpec = {
                 }
             }
         },
-        "dateIssued": {
+        "mods:dateIssued": {
             menu: [{
                 caption: "Add @keydate",
                 action: Xonomy.newAttribute,
@@ -207,14 +211,14 @@ var modsSpec = {
             }
         },
 
-        "genre": {
+        "mods:genre": {
             hasText: true,
             menu: [{
                 caption: "Delete <genre>",
                 action: Xonomy.deleteElement
             }],
         },
-        "identifier": {
+        "mods:identifier": {
             hasText: true,
             menu: [{
                 caption: "Add @displayLabel",
@@ -248,16 +252,16 @@ var modsSpec = {
                 }
             }
         },
-        "name": {
+        "mods:name": {
             menu: [{
                     caption: "Append an <namePart>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<namePart />"
+                    actionParameter: "<mods:namePart  xmlns:mods='http://www.loc.gov/mods/v3'/>"
                 },
                 {
                     caption: "Append an <role>",
                     action: Xonomy.newElementChild,
-                    actionParameter: "<role><roleTerm /></role>"
+                    actionParameter: "<mods:role xmlns:mods='http://www.loc.gov/mods/v3'><mods:roleTerm /></mods:role>"
                 },
                 {
                     caption: "Append @type=\"type_of\"",
@@ -314,7 +318,7 @@ var modsSpec = {
                 }
             }
         },
-        "namePart": {
+        "mods:namePart": {
             menu: [{
                 caption: "Delete <namePart>",
                 action: Xonomy.deleteElement
@@ -322,7 +326,7 @@ var modsSpec = {
             canDropTo: ["name"],
             hasText: true
         },
-        "note": {
+        "mods:note": {
            menu: [{ 
                 caption: "Add @type=\"{type_of}\"",
                 action: Xonomy.newAttribute,
@@ -338,7 +342,7 @@ var modsSpec = {
             {
                 caption: "New <note>",
                 action: Xonomy.newElementAfter,
-                actionParameter: "<note></note>"
+                actionParameter: "<mods:note xmlns:mods='http://www.loc.gov/mods/v3'></note>"
             }],
            attributes: {
                 "displayLabel": {
@@ -365,40 +369,40 @@ var modsSpec = {
             },
             hasText: true
         },
-        "originInfo": {
+        "mods:originInfo": {
             menu: [{
                 caption: "Add <dateCreated>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<dateCreated/>"
+                actionParameter: "<mods:dateCreated xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },{
                 caption: "Add <dateIssued>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<dateIssued />"
+                actionParameter: "<mods:dateIssued  xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },{
                 caption: "Add <publisher>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<publisher />"
+                actionParameter: "<mods:publisher xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },{
                 caption: "Add <place>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<place><placeTerm /></place>"
+                actionParameter: "<mods:place xmlns:mods='http://www.loc.gov/mods/v3'><mods:placeTerm /></mods:place>"
             },{
                 caption: "Delete <originInfo>",
                 action: Xonomy.deleteElement
             }]
         },
-        "place": {
+        "mods:place": {
             menu: [{
                 caption: "Add <placeTerm>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<placeTerm/>"
+                actionParameter: "<mods:placeTerm xmlns:mods='http://www.loc.gov/mods/v3' />"
             },
             {
                 caption: "Delete <place>",
                 action: Xonomy.deleteElement
             }]
         },
-        "placeTerm": {
+        "mods:placeTerm": {
             menu: [{
                 caption: "Add @authority property",
                 action: Xonomy.newAttribute,
@@ -433,39 +437,39 @@ var modsSpec = {
                 }
             }
         },
-        "physicalDescription": {
+        "mods:physicalDescription": {
             menu: [{
                 caption: "Add <digitalOrigin>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<digitalOrigin />"
+                actionParameter: "<mods:digitalOrigin xmlns:mods='http://www.loc.gov/mods/v3' />"
             },{
                 caption: "Add <extent>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<extent />"
+                actionParameter: "<mods:extent xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },{
                 caption: "Add <form>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<form authority=\"marcform\"/>"
+                actionParameter: "<mods:form authority=\"marcform\" xmlns:mods='http://www.loc.gov/mods/v3' />"
             },{
                 caption: "Delete <physicalDescription>",
                 action: Xonomy.deleteElement
             }]
         },
-        "publisher": {
+        "mods:publisher": {
             menu: [{
                 caption: "Delete <publisher>",
                 action: Xonomy.deleteElement
             }],
             hasText: true
         },
-        "role": {
+        "mods:role": {
             menu: [{
                 caption: "Append an <roleTerm>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<roleTerm/>"
+                actionParameter: "<mods:roleTerm/>"
             }]
         },
-        "roleTerm": {
+        "mods:roleTerm": {
             menu: [{
                 caption: "Add @type=\"something\"",
                 action: Xonomy.newAttribute,
@@ -483,7 +487,7 @@ var modsSpec = {
                 }
             }
         },
-        "subject": {
+        "mods:subject": {
             menu: [{
                 caption: "Add @valueURI property",
                 action: Xonomy.newAttribute,
@@ -492,27 +496,27 @@ var modsSpec = {
             {
                 caption: "Add <topic>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<topic/>"
+                actionParameter: "<mods:topic xmlns:mods='http://www.loc.gov/mods/v3' />"
             },
             {
                 caption: "Add <name>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<name><namePart></namePart></name>"
+                actionParameter: "<mods:name xmlns:mods='http://www.loc.gov/mods/v3'><mods:namePart></mods:namePart></mods:name>"
             },
             {
                 caption: "Add <geographic>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<geographic />"
+                actionParameter: "<mods:geographic xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },
             {
                 caption: "Add <temporal>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<temporal />"
+                actionParameter: "<mods:temporal xmlns:mods='http://www.loc.gov/mods/v3' />"
             },
             {
                 caption: "Add new <subject>",
                 action: Xonomy.newElementAfter,
-                actionParameter: "<subject />"
+                actionParameter: "<mods:subject xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },
             {
                 caption: "Delete <subject>",
@@ -530,40 +534,61 @@ var modsSpec = {
             }
 
         },
-        "subTitle": {
+        "mods:subTitle": {
             menu: [{
                 caption: "Delete <subTitle>",
                 action: Xonomy.deleteElement
             }],
             hasText: true
         },
-        "title": {
+        "mods:title": {
             menu: [{
                 caption: "Delete <title>",
                 action: Xonomy.deleteElement
             }],
             hasText: true
         },
-       "titleInfo": {
+       "mods:titleInfo": {
             menu: [{
                 caption: "Add a <title>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<title/>"
+                actionParameter: "<mods:title xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },{
                 caption: "Add a <subTitle>",
                 action: Xonomy.newElementChild,
-                actionParameter: "<subTitle/>"
+                actionParameter: "<mods:subTitle xmlns:mods='http://www.loc.gov/mods/v3'/>"
             },{
                 caption: "Delete <titleInfo>",
                 action: Xonomy.deleteElement
-            }]
+            }],
         },
-        "topic": {
+        "mods:topic": {
             menu: [{
                 caption: "Delete <topic>",
                 action: Xonomy.deleteElement
             }],
             hasText: true
+        },
+        "mods:typeOfResource": {
+            menu: [{
+                caption: "Delete <typeOfResource>",
+                action: Xonomy.deleteElement
+            }],
+            hasText: true,
+            asker: Xonomy.askPicklist,
+            askerParameter: [
+                "text",
+                "cartographic",
+                "notated music",
+                "sound recording",
+                "sound recording-musical",
+                "sound recording-nonmusical",
+                "still image",
+                "moving image",
+                "three dimensional object",
+                "software, multimedia",
+                "mixed material" 
+            ]
         }
     }
 };
